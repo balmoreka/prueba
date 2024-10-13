7031cf1d403c9a072f0e627f2ab73741051dd87d
Balmore H <balmoreka@gmail.com>
2024-10-13 16:59:17 -0600

    Add XGBoost and CatBoost

--- models.py
+++ models.py
@@ -4,11 +4,15 @@ def rf():
 	print("Ejecutando Random Forest")
 def cb():
 	print("Ejecutando CatBoost")
+def xg():
+	print("Ejecutando XGBoost")
 def main():
 	if sys.argv[1] == 'rf':
 		rf()
 	elif sys.argv[1] == 'cb':
 		cb()
+	elif sys.argv[1] == 'xg':
+		xg()
 	else:
 		print("Sin modelo")
 
