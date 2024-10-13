de61a9822f9eeacc3e15895f223c0b2f7e834c5b
Balmore H <balmoreka@gmail.com>
2024-10-13 16:22:42 -0600

    Add Catboost model for prediction

--- models.py
+++ models.py
@@ -2,9 +2,13 @@ import sys
 
 def rf():
 	print("Ejecutando Random Forest")
+def cb():
+	print("Ejecutando CatBoost")
 def main():
 	if sys.argv[1] == 'rf':
 		rf()
+	elif sys.argv[1] == 'cb':
+		cb()
 	else:
 		print("Sin modelo")
 
